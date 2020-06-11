417da9e15236101c80240dc60bc3ac910ccb0fd1
Jcparkyn <51850908+Jcparkyn@users.noreply.github.com>
2020-06-11 16:56:59 +1000

    Add 'copy to clipboard' button

---
+++ RegexNodes/wwwroot/js/clipboardCopy.js
@@ -0,0 +1,13 @@
+﻿//Handle copying text to the user's clipboard.
+//From https://chrissainty.com/copy-to-clipboard-in-blazor/
+
+window.clipboardCopy = {
+    copyText: function (text) {
+        navigator.clipboard.writeText(text).then(function () {
+            //alert("Copied to clipboard!");
+        })
+            .catch(function (error) {
+                alert(error);
+            });
+    }
+};
