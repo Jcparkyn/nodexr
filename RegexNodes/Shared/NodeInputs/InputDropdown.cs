0bd2f3ef95c92e92d4c3093d617d9d9cbe3e0195
Jcparkyn <51850908+Jcparkyn@users.noreply.github.com>
2020-06-24 14:22:50 +1000

    Create generic InputDropdown class

--- RegexNodes/Shared/NodeInputs/InputDropdown.cs
+++ RegexNodes/Shared/NodeInputs/InputDropdown.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 
@@ -7,7 +8,7 @@ namespace RegexNodes.Shared
     public class InputDropdown : NodeInput
     {
         private string dropdownValue;
-        public string DropdownValue
+        public virtual string DropdownValue
         {
             get => dropdownValue;
             set
@@ -17,12 +18,44 @@ namespace RegexNodes.Shared
             }
         }
 
-        public List<string> Options { get; private set; }
+        public virtual List<string> Options { get; private set; }
 
         public InputDropdown(params string[] options)
         {
             DropdownValue = options[0];
             Options = options.ToList();
+        }
+    }
+
+    public class InputDropdown<TValue> : InputDropdown
+        where TValue : Enum
+    {
+        //private string dropdownValue;
+        private Dictionary<TValue, string> displayNames;
+
+        public TValue Value { get; set; }
+
+        public override string DropdownValue
+        {
+            get => displayNames[Value];
+            set
+            {
+                Value = displayNames.FirstOrDefault(x => x.Value == value).Key;
+                OnValueChanged();
+            }
+        }
+
+        public override List<string> Options
+        {
+            get => displayNames.Values.ToList();
+        }
+
+
+        public InputDropdown(Dictionary<TValue, string> displayNames)
+        {
+            this.displayNames = displayNames;
+            //DropdownValue = options[0];
+            //Options = options.ToList();
         }
     }
 }
