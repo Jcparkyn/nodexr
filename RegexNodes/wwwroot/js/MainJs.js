5a01a21328f46da9d56d97a71a9d9c1a4ec948ac
Jcparkyn <51850908+Jcparkyn@users.noreply.github.com>
2020-07-07 13:23:14 +1000

    Add help info to default search text

--- RegexNodes/wwwroot/js/MainJs.js
+++ RegexNodes/wwwroot/js/MainJs.js
@@ -4,13 +4,13 @@ window.jsFunctions = {
     blazorHasRendered: function () {
         console.log("splitting windows");
         Split(['#viewport-searchtext', '#viewport-replacetext', '#viewport-replaceresult'], {
-            sizes: [40, 30, 30],
+            sizes: [49, 15, 36],
             gutterSize: 7,
             snapOffset: 0,
             minSize: [250, 250, 250],
         })
         Split(['#mainpanels', '#bottompanels'], {
-            sizes: [85, 15],
+            sizes: [80, 20],
             direction: 'vertical',
             gutterSize: 7,
             snapOffset: 0,
