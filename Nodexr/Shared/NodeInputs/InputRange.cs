4acb65910eeca066913b96bf9408bd2e1386d032
Jcparkyn <51850908+Jcparkyn@users.noreply.github.com>
2020-10-11 19:05:40 +1100

    Add InputRange input type

---
+++ Nodexr/Shared/NodeInputs/InputRange.cs
@@ -0,0 +1,45 @@
+﻿namespace Nodexr.Shared.NodeInputs
+{
+    public class InputRange : NodeInput
+    {
+        private int? min;
+
+        public int? Min
+        {
+            get => min;
+            set
+            {
+                min = value;
+                OnValueChanged();
+            }
+        }
+
+        private int? max;
+
+        public int? Max
+        {
+            get => max;
+            set
+            {
+                max = value;
+                OnValueChanged();
+            }
+        }
+
+        public int? MinValue { get; set; }
+
+        public override int Height => 50;
+
+        public InputRange(int? min = null, int? max = null, int? minValue = null)
+        {
+            Min = min;
+            Max = max;
+            MinValue = minValue;
+        }
+
+        public (int?, int?) GetValue()
+        {
+            return (min, max);
+        }
+    }
+}
