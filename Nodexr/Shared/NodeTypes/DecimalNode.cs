40c99cd32dbcc6cb9a6f564d941e45bdec649278
Jcparkyn <51850908+Jcparkyn@users.noreply.github.com>
2020-10-11 18:09:01 +1100

    Add help text to DecimalNode

--- Nodexr/Shared/NodeTypes/DecimalNode.cs
+++ Nodexr/Shared/NodeTypes/DecimalNode.cs
@@ -11,7 +11,13 @@ namespace Nodexr.Shared.NodeTypes
     {
         public override string Title => "Decimal";
 
-        public override string NodeInfo => "Matches a decimal/floating point number."; //TODO
+        public override string NodeInfo => "Matches a decimal/floating point number. " +
+            "\nThe digit separator will not enforce that digits are strictly in groups of three, " +
+            "but the match will never start with a separator." +
+            "\nIf 'Optional Decimal' and 'Optional Integer' are both checked, the expression will " +
+            "only match when at least one of them is found." +
+            "\n\nWarning: this node is marked as 'Experimental' because it will not be preserved " +
+            "after using the 'Create Link' or 'Edit' buttons.";
 
         [NodeInput]
         public InputDropdown<SignType> InputSign { get; } = new InputDropdown<SignType>()
@@ -21,26 +27,47 @@ namespace Nodexr.Shared.NodeTypes
         };
 
         [NodeInput]
-        public InputString InputDecimalSeparator { get; } = new InputString(".") { Title = "Decimal Separator(s)" };
+        public InputString InputDecimalSeparator { get; } = new InputString(".")
+        {
+            Title = "Decimal Separator(s)",
+            Description = "Character to separate the integer part of the number from the decimal. " +
+                "If more than one character is entered, the expression will match any one of them."
+        };
 
         [NodeInput]
-        public InputString InputThousandsSeparator { get; } = new InputString("") { Title = "Digit Separator(s)" };
+        public InputString InputThousandsSeparator { get; } = new InputString("")
+        {
+            Title = "Digit Separator(s)",
+            Description = "Character to separate groups of digits in the integer part of the number. Leave blank for no separator."
+        };
 
         [NodeInput]
         public InputDropdown<CaptureType> InputCaptureType { get; } = new InputDropdown<CaptureType>(captureTypeDisplayNames)
         {
             Title = "Capture?",
-            Description = "Store the result using a capturing group."
+            Description = "Store the result using a capturing group?"
         };
 
         [NodeInput]
-        public InputString InputGroupName { get; } = new InputString("") { Title = "Capture Name" };
+        public InputString InputGroupName { get; } = new InputString("")
+        {
+            Title = "Capture Name",
+            Description = "The name to give to the captured group."
+        };
 
         [NodeInput]
-        public InputCheckbox InputOptionalDecimal { get; } = new InputCheckbox() { Title = "Optional Decimal" };
+        public InputCheckbox InputOptionalDecimal { get; } = new InputCheckbox()
+        {
+            Title = "Optional Decimal",
+            Description = "If checked, numbers with no decimal part (e.g. 10) are matched."
+        };
 
         [NodeInput]
-        public InputCheckbox InputOptionalInteger { get; } = new InputCheckbox() { Title = "Optional Integer" };
+        public InputCheckbox InputOptionalInteger { get; } = new InputCheckbox()
+        {
+            Title = "Optional Integer",
+            Description = "If checked, numbers with no integer part (e.g. .5) are matched."
+        };
 
         public enum CaptureType
         {
@@ -77,11 +104,14 @@ namespace Nodexr.Shared.NodeTypes
                 _ => "",
             };
 
-            string decimalSeparator = "[" + InputDecimalSeparator.GetValue() + "]";
+            //decimal separator should fall back to \. if left empty.
+            string decimalSeparator = string.IsNullOrEmpty(InputDecimalSeparator.GetValue()) ?
+                "\\." :
+                "[" + InputDecimalSeparator.GetValue() + "]";
 
-            string number;
+            string number; //Expression to match the number, without sign
+
             bool noThousandsSeparator = string.IsNullOrEmpty(InputThousandsSeparator.GetValue());
-
             if (noThousandsSeparator)
             {
                 number = (InputOptionalDecimal.IsChecked, InputOptionalInteger.IsChecked) switch
