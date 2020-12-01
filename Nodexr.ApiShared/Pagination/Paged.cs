70113a60c203f7c374339500f8dc84998b4b1618
Jcparkyn <51850908+Jcparkyn@users.noreply.github.com>
2020-12-01 19:26:08 +1100

    Add ApiShared and Paged<T>

---
+++ Nodexr.ApiShared/Pagination/Paged.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Nodexr.ApiShared.Pagination
+{
+    public class Paged<T>
+    {
+        public Paged(List<T> contents, int totalSize, int start, int limit)
+        {
+            Contents = contents;
+            TotalSize = totalSize;
+            Start = start;
+            Limit = limit;
+        }
+
+        public List<T> Contents { get; }
+        public int TotalSize { get; set; }
+        public int Start { get; set; }
+        public int Limit { get; set; }
+    }
+}
+
