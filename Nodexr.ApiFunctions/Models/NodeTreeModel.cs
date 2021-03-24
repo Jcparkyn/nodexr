3374808b84bc17ee2e47374cfa781da5ad552636
Jcparkyn <51850908+Jcparkyn@users.noreply.github.com>
2021-03-24 11:26:15 +1100

    Make Title property of NodeTreeModel non-nullable.

--- Nodexr.ApiFunctions/Models/NodeTreeModel.cs
+++ Nodexr.ApiFunctions/Models/NodeTreeModel.cs
@@ -9,10 +9,15 @@ namespace Nodexr.ApiFunctions.Models
     {
         public string? id { get; set; } = Guid.NewGuid().ToString();
 
-        public string? Title { get; set; }
+        public string Title { get; set; }
 
         public string? Expression { get; set; }
 
         public string? Description { get; set; }
+
+        public NodeTreeModel(string title)
+        {
+            Title = title;
+        }
     }
 }
