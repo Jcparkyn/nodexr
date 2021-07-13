4cddbf2fcfa10baacb590edb8fb5d26cf2e45c9f
Jcparkyn <51850908+Jcparkyn@users.noreply.github.com>
2021-07-13 18:19:31 +1000

    Replace <app> with <div id="app">

--- Nodexr/Program.cs
+++ Nodexr/Program.cs
@@ -22,7 +22,7 @@ namespace Nodexr
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddBlazoredToast();
             builder.Services.AddTransient(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
